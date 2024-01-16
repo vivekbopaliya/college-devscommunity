bc157bfa4b905df9ed036e1754a58d562a4ee4d9
Vivek <vivekpatel1nov@gmail.com>
2024-01-16 18:48:48 +0530

    hackathon timeline added

--- src/app/hackathon/page.tsx
+++ src/app/hackathon/page.tsx
@@ -16,7 +16,7 @@ import {
 const page = () => {
   const route = useRouter();
   return (
-    <main className="w-screen sm:-ml-28 -ml-3 sm:py-28 py-36 -mt-16 h-full flex justify-center items-center hackathon__bg">
+    <main className="w-screen sm:-ml-28 -ml-3 sm:py-28 py-36 -mt-16 h-full flex flex-col justify-center items-center hackathon__bg">
       <div className="  flex flex-col justify-center items-center">
         <div className="flex flex-col items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium">
           <p>⌚ Date: 2nd - 3rd March </p>
@@ -109,6 +109,80 @@ const page = () => {
           </Button>
         </main>
       </div>
+
+      <section className="py-10 events flex flex-col justify-center items-center">
+        <p>Total timeline for Code Carnival participants</p>
+        <h1 className="sm:text-5xl text-4xl font-extrabold pb-16 pt-2">
+          <span className="red">W</span>hat's <span className="green">C</span>
+          oming <span className="blue">U</span>p
+          <span className="yellow">..?</span>
+        </h1>
+        <ul>
+          {/* @ts-ignore */}
+          <li style={{ "--accent-color": "#EA4335" }}>
+            <div className="date text-3xl">Registration</div>
+            <div className="title"> 10/01/2024 - 10/02/2024</div>
+            <div className="descr">
+              {" "}
+              The registration phase marks the initial step for participants to
+              express their interest in joining the hackathon. During this
+              period, individuals or teams register for the event by providing
+              necessary information, such as team details, participant names,
+              and any required background information. Organizers often use
+              online platforms or dedicated websites to streamline the
+              registration process and gather essential data.
+            </div>
+          </li>
+
+          {/* @ts-ignore */}
+          <li style={{ "--accent-color": "#0F9D58" }}>
+            <div className="date text-3xl">Presentation Submission</div>
+            <div className="title"> 1/02/2024 - 15/02/2024</div>
+            <div className="descr">
+              {" "}
+              Following successful registration, participants are typically
+              required to prepare and submit their project proposals or
+              presentation materials. This phase is crucial for teams to outline
+              their project ideas, objectives, and any preliminary work they
+              have done. Presentation submission allows organizers to review and
+              assess the proposed projects, ensuring they align with the
+              hackathon's theme and guidelines.
+            </div>
+          </li>
+          {/* @ts-ignore */}
+
+          <li style={{ "--accent-color": "#4285F4" }}>
+            <div className="date text-3xl">Acceptance Notification</div>
+            <div className="title">17/02/2024</div>
+            <div className="descr">
+              {" "}
+              After the presentation submission deadline, organizers review the
+              entries to select the most promising projects that will proceed to
+              the hackathon event. The acceptance notification phase involves
+              notifying participants or teams about the outcome of the selection
+              process. Successful participants receive confirmation of their
+              acceptance into the hackathon, while those who are not selected
+              may receive feedback or information on future opportunities.
+            </div>
+          </li>
+          {/* @ts-ignore */}
+
+          <li style={{ "--accent-color": "#F8A218" }}>
+            <div className="date text-3xl">Final Details Declaration</div>
+            <div className="title"> 20/02/2024</div>
+            <div className="descr">
+              As the hackathon date approaches, organizers release final details
+              to the accepted participants. This includes essential information
+              such as the event schedule, venue details (whether it's in-person
+              or virtual), rules and guidelines, tools or platforms to be used,
+              and any additional resources or support provided during the
+              hackathon. The final details declaration ensures that participants
+              are well-prepared and have all the necessary information to make
+              the most of the hackathon experience.
+            </div>
+          </li>
+        </ul>
+      </section>
     </main>
   );
 };
