c0fc828c983dd6b4db21a55c160d590b23122926
Vivek <vivekpatel1nov@gmail.com>
2024-01-20 12:03:57 +0530

    gfd

--- src/app/api/hackathon/route.ts
+++ src/app/api/hackathon/route.ts
@@ -4,8 +4,11 @@ export async function POST(req: Request) {
   const body = await req.json();
 
   try {
-    await axios.post(process.env.HACKATHON_URL!, body);
-    return new Response("ok");
+    await axios.post(
+      "https://sheet.best/api/sheets/e82b0fc1-2539-4756-967c-46337b25b4cf",
+      body
+    );
+    return new Response("okay");
   } catch (error: any) {
     return new Response(error, { status: 500 });
   }
