e980edbdd10e2b6413ba81aa974fbd9c50a94f17
Vivek <vivekpatel1nov@gmail.com>
2024-02-27 11:05:43 +0530

    register form reopened

--- src/app/hackathon/form/layout.tsx
+++ src/app/hackathon/form/layout.tsx
@@ -12,25 +12,25 @@ interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-// const sidebarNavItems = [
-//   {
-//     title: "Team Leader",
-//     href: "/hackathon/form",
-//   },
-//   {
-//     title: "Team Member 1",
-//     href: "/hackathon/form/member1",
-//   },
-//   {
-//     title: "Team Member 2",
-//     href: "/hackathon/form/member2",
-//   },
+const sidebarNavItems = [
+  {
+    title: "Team Leader",
+    href: "/hackathon/form",
+  },
+  {
+    title: "Team Member 1",
+    href: "/hackathon/form/member1",
+  },
+  {
+    title: "Team Member 2",
+    href: "/hackathon/form/member2",
+  },
 
-//   {
-//     title: "Team Member 3",
-//     href: "/hackathon/form/member3",
-//   },
-// ];
+  {
+    title: "Team Member 3",
+    href: "/hackathon/form/member3",
+  },
+];
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
@@ -38,20 +38,28 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
       <div className=" space-y-6 -mt-5 sm:p-10 p-5 sm:pb-16 pb-0 md:block">
         <div className="flex gap-3">
           <div>
-            <BadgeAlert size={50} className="fill-red-600" />
+            <BadgeAlert size={40} className="fill-blue-600" />
           </div>
           <div className="space-y-0.5">
+<<<<<<< HEAD
             <h2 className="text-3xl text-red-600 font-bold tracking-tight">
               Register Closed!
+=======
+            <h2 className="text-2xl text-blue-600 font-bold tracking-tight">
+              Register for Code Carnival
+>>>>>>> parent of f15157c (register closed)
             </h2>
             <p className="text-muted-foreground">
-              The registration deadline has passed. the register was closed on
-              February 19th.
+              Fill out the form step by step, adding details for each team
+              member as needed.
             </p>
           </div>
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 text-xs lg:text-base lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
         </div>
       </div>
